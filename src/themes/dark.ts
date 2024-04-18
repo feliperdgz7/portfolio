5bcfc6740b862371f252b1c4dbf017f5a053f8f9
Felipe Rodrigues <arthurdz741@gmail.com>
2024-04-18 12:11:35 -0300

    criaçao de thema light e dark

---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+const temaDark = {
+  corPrincipal: '#FFF',
+  corSecundaria: '#eee',
+  corFundo: '#282a35',
+  corDeFundoBotao: '#fff',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaDark
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corFundo: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
